3edc208e9c85ac0751d9d0f28b4c701769ee2792
Isra <isra.jawaid@pwc.com>
2021-04-19 17:40:37 +0100

    Added import file and reset file feature

--- src/components/ReadFileList.vue
+++ src/components/ReadFileList.vue
@@ -1,6 +1,18 @@
 <template>
     <v-container>
-        <v-btn elevation="2">Import File</v-btn>
+        <v-row>
+            <v-btn elevation="2" @click="uploadFile()">Import File</v-btn>
+            <v-spacer></v-spacer>
+            <v-btn elevation="2" @click="resetFile()">Reset File</v-btn>
+        </v-row>
+        <v-file-input 
+        v-model="myFile"
+        prepend-icon=""
+        accept=".json"
+        class="hide"
+        id="inputFile"
+        @change="readFile"
+        ></v-file-input>
         <v-data-table
             :headers = "headers"
             :items = "items"
@@ -14,35 +26,43 @@
             <template v-slot:body="{ items }">
                 <tbody>
                     <tr v-for="item in items" :key="item.Fileroot">
-                        <td @click="clicked(item, 'Fileroot')">{{ item.Fileroot }}</td>
-                        <td @click="clicked(item, 'Action')">{{ item.Action }}</td>
-                        <td @click="clicked(item, 'Schema')">{{ item.Schema.toString().slice(0,50) }}</td>
-                        <td @click="clicked(item, 'DataType')">{{ item.DataType.toString().slice(0,50) }}</td>
-                        <td @click="clicked(item, 'Table')">{{ item.Table }}</td>
-                        <td @click="clicked(item, 'Primary_Key')">{{ item.Primary_Key }}</td>
-                        <td @click="clicked(item, 'Hash_Keys')">{{ item.Hash_Keys.toString().slice(0,50) }}</td>
+                        <td @click="clicked(item, 'Fileroot', false)">{{ item.Fileroot }}</td>
+                        <td @click="clicked(item, 'Action', false)">{{ item.Action }}</td>
+                        <td @click="clicked(item, 'Schema', false)">{{ item.Schema.toString().slice(0,50) }}</td>
+                        <td @click="clicked(item, 'DataType', false)">{{ item.DataType.toString().slice(0,50) }}</td>
+                        <td @click="clicked(item, 'Table', false)">{{ item.Table }}</td>
+                        <td @click="clicked(item, 'Primary_Key', false)">{{ item.Primary_Key }}</td>
+                        <td @click="clicked(item, 'Hash_Keys', false)">{{ item.Hash_Keys.toString().slice(0,50) }}</td>
                     </tr>
                 </tbody>
             </template>
         </v-data-table>
 
-        <v-btn elevation="2" @click="newRecord">Create New Record</v-btn>
+        <v-row>
+            <v-btn elevation="2" @click="newRecord">Create New Record</v-btn>
+            <v-spacer></v-spacer>
+            <v-btn elevation="2">Save as new file</v-btn>
+        </v-row>
 
 
-
-        <v-dialog v-model="dialog" width="500">
+        <v-dialog v-model="dialog" width="500" v-bind:persistent="persistentDialog">
             <v-card>
                 <v-card-title>
                     Edit {{editAttribute}}
                 </v-card-title>
                 <v-card-text>
-                    <v-textarea v-if="showBigInput"
-                    v-model="editedItem[editAttribute]"
-                    auto-grow
-                    autofocus
-                    rows="1"
-                    ></v-textarea>
-                    <v-text-field v-else v-model="editedItem[editAttribute]"></v-text-field>
+                    <v-form ref="form">
+                        <v-textarea v-if="showBigInput"
+                        v-model="editedItem[editAttribute]"
+                        auto-grow
+                        autofocus
+                        rows="1"
+                        ></v-textarea>
+                        <v-text-field v-else v-model="editedItem[editAttribute]"
+                        :rules="rules"
+                        hide-details="auto"
+                        ></v-text-field>
+                    </v-form>
                 </v-card-text>
                 <v-card-actions>
                     <v-btn
@@ -71,7 +91,7 @@ import filelist from '../assets/filelist.json'
 export default {
     name: 'ReadFileList',
     data: () => ({
-        myFile: filelist,
+        myFile: [],
         reRender: 0,
         dialog: false,
         itemsPerPage: 10,
@@ -81,10 +101,11 @@ export default {
         editedItem: '',
         editAttribute: '',
         showBigInput: false,
+        persistentDialog: false,
         headers: [
             { text:'File Root', value: 'Fileroot'},
             { text:'Action', value: 'Action'},
-            { text:'Schema', value: 'Schema', width: '1%'},
+            { text:'Schema', value: 'Schema'},
             { text:'Data Type', value: 'DataType'},
             { text:'Table', value: 'Table'},
             { text:'Primary Key', value: 'Primary_Key'},
@@ -92,22 +113,46 @@ export default {
         ],
         items: [],
     }),
+    computed: {
+        rules () {
+            const rules = []
+            if(this.editAttribute = 'Fileroot') {
+                const rule1 = value => !!value || 'Required.'
+                rules.push(rule1)
+                var fileRoots = []
+                for (var i of this.items) {
+                    fileRoots.push(i['Fileroot'])
+                }
+                const rule2 = value => (this.itemToEdit['Fileroot'] == value) || (fileRoots.indexOf(value) == -1) || 'Duplicate File Root'
+                rules.push(rule2)
+            }
+            return rules
+        }
+    },
     methods: {
         readFile(file) {
-            for (var item of file) {
-                var d = {}
-                for (var header of this.headers) {
-                    d[header['value']] = item[header['value']]
-                }
-                this.items.push(d)
-            }
+            this.asyncReadFile(file).then(result => {
+                var data = JSON.parse(result)
+                this.items = Object.values(data)
+                this.myFile = Object.values(data)
+                console.log(this.myFile)
+                this.forceRenderer()
+            })
+            
+        },
+        async asyncReadFile(file) {
+            let result = await new Promise((resolve) => {
+                let fileReader = new FileReader()
+                fileReader.onload = (e) => resolve(fileReader.result)
+                fileReader.readAsText(file)
+            })
+            return result
         },
         forceRenderer() {
             this.reRender += 1
         },
-        clicked(item, attribute) {
-            console.log("Clicked")
-            this.dialog = true
+        clicked(item, attribute, persistent) {
+            this.persistentDialog = persistent
             this.itemToEdit = item
             this.editedItem = Object.assign({}, this.itemToEdit)
             this.editAttribute = attribute
@@ -117,23 +162,23 @@ export default {
             else {
                 this.showBigInput = false
             }
-            console.log(item)
-            console.log(this.itemsPerPage)
+            this.dialog = true
         },
         saveAttribute() {
-            this.dialog = false
-            if (this.editAttribute == 'Schema' || this.editAttribute == 'DataType' || this.editAttribute == 'Hash_Keys') {
-                this.editedItem[this.editAttribute] = (this.editedItem[this.editAttribute]).split(",")
-            }
-            for (var i of this.items) {
-                if (i == this.itemToEdit) {
-                    this.items[this.items.indexOf(i)] = this.editedItem
-                    
+            if (this.$refs.form.validate()) {
+                this.dialog = false
+                if (this.itemToEdit[this.editAttribute] != this.editedItem[this.editAttribute]) {
+                    if (this.editAttribute == 'Schema' || this.editAttribute == 'DataType' || this.editAttribute == 'Hash_Keys') {
+                        this.editedItem[this.editAttribute] = (this.editedItem[this.editAttribute]).split(",")
+                    }
                 }
+                for (var i of this.items) {
+                    if (i == this.itemToEdit) {
+                        this.items[this.items.indexOf(i)] = this.editedItem
+                    }
+                }
+                this.forceRenderer()
             }
-            if (this.editAttribute == 'Schema' || this.editAttribute == 'DataType' || this.editAttribute == 'Hash_Keys') {}
-            console.log(this.items)
-            this.forceRenderer()
         },
         resetAttribute() {
             this.editedItem[this.editAttribute] = this.itemToEdit[this.editAttribute]
@@ -145,22 +190,39 @@ export default {
             }
             this.items.push(newItem)
             this.forceRenderer()
+            if (Math.ceil(this.items.length / this.itemsPerPage) > this.pageCount) {
+                this.pageCount += 1
+            }
             this.page = this.pageCount
+            this.clicked(newItem, 'Fileroot', true)
+            
         },
         paginate(val) {
             this.page = val.page
             this.itemsPerPage = val.itemsPerPage
             this.pageCount = val.pageCount
+        },
+        uploadFile() {
+            document.getElementById("inputFile").click()
+        },
+        resetFile() {
+            this.items = JSON.parse(JSON.stringify(this.myFile))
+            this.forceRenderer()
+            this.page = 1
         }
     },
     mounted() {
-        this.readFile(this.myFile)
+        
     }
 }
 </script>
 
-<style>
+<style scoped>
     .v-btn {
-        margin: 1em;
+        margin: 2em 1em;
+    }
+
+    .hide {
+        display: none!important;
     }
 </style>
